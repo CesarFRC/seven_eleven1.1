8e4afd62fa0a1f3c5f639231341301f72b3a3c09
CesarFrc <cesar.reyes.cortinas12@gmail.com>
2025-11-25 12:30:23 -0600

    Ya tiene la musica los demas

---
+++ seven_eleven/InstrucionesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InstrucionesViewController.swift
+//  seven_eleven
+//
+//  Created by Erik Fernando Hernàndez Ayala on 24/11/25.
+//
+
+import UIKit
+
+class InstrucionesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
