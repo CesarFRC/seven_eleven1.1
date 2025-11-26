0efa4f43328c58b958dd9b703b8867a9eff0b4d4
CesarFrc <cesar.reyes.cortinas12@gmail.com>
2025-11-25 23:46:20 -0600

    Ya se dividio el sonido de las pantallas, jugar tiene su propio sonido independiente, tanto menu como instruciones y records tienen el mismo sonido. Solo faltaria en poner algo como que siga en la misma sintonia o que se detenga una y inicie la otra

---
+++ seven_eleven/MusicaViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MusicaViewController.swift
+//  seven_eleven
+//
+//  Created by Erik Fernando Hernàndez Ayala on 25/11/25.
+//
+
+import UIKit
+
+class MusicaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
